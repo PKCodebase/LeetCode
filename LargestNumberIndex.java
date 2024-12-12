40ff32d8ffacf213eeaff6a10f319b72b8a7fb15
PKCodebase <kaushikprasad1659@gmail.com>
2024-12-12 21:31:03 +0530

    LargestNumberIndex

---
+++ LargestNumberIndex.java
@@ -0,0 +1,33 @@
+package com.leetcode;
+
+public class LargestNumberIndex {
+
+        public int dominantIndex(int[] nums) {
+            int max = -1;
+            int smax = -1;
+
+            int maxIdx = 0;
+            for(int i=0;i<nums.length;i++){
+                if(max<nums[i]){
+                    smax = max;
+                    max = nums[i];
+                    maxIdx = i;
+
+                }else if(smax < nums[i]){
+                    smax = nums[i];
+                }
+            }
+            if(smax * 2<=max){
+                return maxIdx;
+            }else{
+                return -1;
+            }
+        }
+        public static void main(String[] args) {
+            int [] nums = {3,6,1,0};
+            LargestNumberIndex largestNumberIndex = new LargestNumberIndex();
+            int result = largestNumberIndex.dominantIndex(nums);
+            System.out.println(result);
+        }
+
+}

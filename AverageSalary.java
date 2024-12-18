01fbc5ef2156e0332097ae9621269142e81aba0a
PKCodebase <kaushikprasad1659@gmail.com>
2024-12-18 21:55:44 +0530

    AverageSalary

---
+++ AverageSalary.java
@@ -0,0 +1,29 @@
+package com.leetcode;
+
+public class AverageSalary {
+        public double average(int[] salary) {
+            int max = salary[0];
+            int min = salary[0];
+            int sum = salary[0];
+            for(int i = 1;i<salary.length;i++){
+
+                if(max < salary[i]){
+                    max = salary[i];
+                }
+
+                if(min > salary[i]){
+                    min = salary[i];
+                }
+                sum = sum + salary[i];
+            }
+            sum = sum - min - max;
+            double result = (double)sum/(salary.length-2);
+            return result;
+        }
+        public static void main(String[] args) {
+            int [] salary = {4000,3000,1000,2000};
+            AverageSalary averageSalary = new AverageSalary();
+            double result = averageSalary.average(salary);
+            System.out.println(result);
+        }
+}

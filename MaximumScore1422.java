9cfaacc058dae898625ac63895e2bfeccf382310
PKCodebase <kaushikprasad1659@gmail.com>
2025-01-04 17:12:34 +0530

    MaximumScore1422

---
+++ MaximumScore1422.java
@@ -0,0 +1,28 @@
+package com.leetcode;
+
+public class MaximumScore1422 {
+    //find all the ones
+    public int maxScore(String s) {
+        int ones = 0;
+        int n = s.length();
+        for(int i = 0 ; i<n;i++){
+            if(s.charAt(i) == '1')
+                ones++;
+        }
+        int res = 0;
+        int zeros = 0;
+        for(int i = 0;i<n-1;i++){
+            if(s.charAt(i) == '0') zeros++;
+            else ones--;
+            res  = Math.max(res,zeros+ones);
+        }
+        return res ;
+    }
+    public static void main(String[] args) {
+        String s = "011101";
+        MaximumScore1422 maximumScore1422 = new MaximumScore1422();
+        int result = maximumScore1422.maxScore(s);
+        System.out.println(result);
+    }
+
+}

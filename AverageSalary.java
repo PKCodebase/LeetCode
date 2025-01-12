10168ad6224afa285d24c510dacbd8a062af939d
PKCodebase <kaushikprasad1659@gmail.com>
2025-01-12 23:02:30 +0530

    AverageSalary

--- AverageSalary.java
+++ AverageSalary.java
@@ -6,11 +6,9 @@ public class AverageSalary {
             int min = salary[0];
             int sum = salary[0];
             for(int i = 1;i<salary.length;i++){
-
                 if(max < salary[i]){
                     max = salary[i];
                 }
-
                 if(min > salary[i]){
                     min = salary[i];
                 }

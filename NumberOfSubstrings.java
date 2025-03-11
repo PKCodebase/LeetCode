9070a859d94855aeb52ea0aea613447a108deb89
PKCodebase <kaushikprasad1659@gmail.com>
2025-03-11 20:10:27 +0530

    NumberOfSubstrings

---
+++ NumberOfSubstrings.java
@@ -0,0 +1,41 @@
+package com.leetcode;
+
+public class NumberOfSubstrings {
+    public static int numberOfSubstrings(String s) {
+        int freq []= new int [3];
+        int count = 0;
+        int curCount= 0;
+        int left = 0;
+        int n = s.length();
+        for(int right=0;right<n;right++){
+            char ch = s.charAt(right);
+            freq[ch-'a']++;
+
+
+            if(freq[ch-'a']==1){
+                curCount++;
+            }
+
+            while(curCount==3){
+                count += (n-right);
+
+                char c = s.charAt(left);
+
+                freq[c-'a']--;
+                if(freq[c-'a']==0){
+                    curCount--;
+                }
+
+                left++;
+            }
+
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        String s = "abca";
+        int result = numberOfSubstrings(s);
+        System.out.println(result);
+    }
+
+}
